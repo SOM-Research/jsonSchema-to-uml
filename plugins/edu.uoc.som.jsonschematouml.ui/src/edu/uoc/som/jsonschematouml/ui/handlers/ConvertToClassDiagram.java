f9dcbc819e824adb4b644e02838c9254432a47a5
Javier Canovas <me@jlcanovas.es>
2018-03-22 11:41:41 +0100

    Preparing 0.0.3

--- plugins/edu.uoc.som.jsonschematouml.ui/src/edu/uoc/som/jsonschematouml/ui/handlers/ConvertToClassDiagram.java
+++ plugins/edu.uoc.som.jsonschematouml.ui/src/edu/uoc/som/jsonschematouml/ui/handlers/ConvertToClassDiagram.java
@@ -8,6 +8,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -21,13 +22,14 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import edu.uoc.som.jsonschematouml.generators.JSONSchemaToUML;
+import edu.uoc.som.jsonschematouml.generators.JSONSchemaToUMLException;
 import edu.uoc.som.jsonschematouml.ui.JSONSchemaToUMLUIPlugin;
 
 /**
  * Main handler class to respond to the main contribution to Eclipse UI. This handler
  * executes the JSON Schema to UML tool for the set of files selected by the user
  *
- */
+ */ 
 public class ConvertToClassDiagram extends AbstractHandler {
 
 	/**
@@ -48,27 +50,18 @@ public class ConvertToClassDiagram extends AbstractHandler {
 						monitor.beginTask("Generating the Class diagram", IProgressMonitor.UNKNOWN);
 						for (Iterator<?> iterator = structuredSelection.iterator(); iterator.hasNext();) {
 							Object obj = iterator.next();
-							if (obj instanceof IFile) {
-								IFile iFile = (IFile) obj;
-								IContainer target = iFile.getProject().getFolder("src-gen");
-								if (!target.getLocation().toFile().exists()) {
-									target.getLocation().toFile().mkdirs();	
-									iFile.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
-								}
-								JSONSchemaToUML generator = new JSONSchemaToUML("test");
-								generator.launch(new File(iFile.getLocation().toString()));
-								
-								String location = target.getFullPath().toString();
-								String fileName = iFile.getName().substring(0, iFile.getName().lastIndexOf('.'));
-						        URI finalLocation = URI.createPlatformResourceURI(location, true).appendSegment(fileName).appendFileExtension("uml");
-						        System.out.println(finalLocation.toString());
+							if (obj instanceof IFile || obj instanceof IFolder) {
+								IResource iResource = (IResource) obj;
+								JSONSchemaToUML generator = new JSONSchemaToUML(iResource.getProject().getName());
+								generator.launch(new File(iResource.getLocation().toString()));
+						        URI finalLocation = buildFinalLocation(iResource, monitor);
 								generator.saveModel(finalLocation);
-								
-							}
+							} 
 						}
 					} catch (CoreException e) {
 						return new Status(IStatus.ERROR, JSONSchemaToUMLUIPlugin.PLUGIN_ID, e.getLocalizedMessage(), e);
-						
+					} catch(JSONSchemaToUMLException je) {
+						return new Status(IStatus.ERROR, JSONSchemaToUMLUIPlugin.PLUGIN_ID, je.getMessage(), je);
 					} finally {
 						monitor.done();
 					}
@@ -76,8 +69,29 @@ public class ConvertToClassDiagram extends AbstractHandler {
 				}
 			};
 			job.schedule();
-			
-		}
+		} 
 		return null;
+	}
+	
+	/**
+	 * Builds and URI for the final destination of the generated Class Diagram
+	 * 
+	 * @param iResource The resource to use as input for the generator
+	 * @param monitor The Monitor of the process
+	 * @return The URI for the target Class Diagram
+	 * @throws CoreException 
+	 */
+	private URI buildFinalLocation(IResource iResource, IProgressMonitor monitor) throws CoreException {
+		IContainer target = iResource.getProject().getFolder("src-gen");
+		if (!target.getLocation().toFile().exists()) {
+			target.getLocation().toFile().mkdirs();	
+			iResource.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+		}		
+		String location = target.getFullPath().toString();
+		String fileName = iResource.getName();
+		if(iResource.getName().lastIndexOf(".") > 0) 
+			fileName = iResource.getName().substring(0, iResource.getName().lastIndexOf('.'));
+        URI finalLocation = URI.createPlatformResourceURI(location, true).appendSegment(fileName).appendFileExtension("uml");
+        return finalLocation;
 	}
 }
